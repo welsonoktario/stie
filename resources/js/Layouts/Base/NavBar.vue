dc35418571d39584923dfd0245daa1f393822684
erlanggagatum <erlanggagatum@gmail.com>
2022-01-27 14:11:34 +0800

    preparation

--- resources/js/Layouts/Base/NavBar.vue
+++ resources/js/Layouts/Base/NavBar.vue
@@ -95,8 +95,8 @@
               class="px-4 py-2 flex flex-row content-center capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out"
               href="/logout"
               method="post"
-              as="button"
-              type="button"
+              as="link"
+              type="link"
             >
               <LogoutIcon class="w-4 h-4 my-auto mr-2" />
               <span>log out</span>
