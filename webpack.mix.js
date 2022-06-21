83a98ca64dfeb66dc71d5325e03befa33f482f98
Welson Oktario <welsonoktario@yahoo.com>
2022-06-22 02:06:08 +0700

    Fix asset url 2

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,9 @@ const mix = require("laravel-mix")
 mix
   .css("resources/css/sheets-of-paper.css", "public/css")
   .js("resources/js/app.js", "public/js")
-  .vue()
+  .vue({
+    extractStyles: true,
+  })
   .postCss("resources/css/app.css", "public/css", [
     require("postcss-import"),
     require("tailwindcss"),
